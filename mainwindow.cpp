6e509eb33e268bcbeb085ce9d2095f1939d907cd
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2021-12-14 06:22:52 +0100

    added Background widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "qpainter.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
---
+++ renderarea.cpp
@@ -0,0 +1,28 @@
+#include "renderarea.h"
+
+RenderArea::RenderArea(QWidget *parent) : QWidget(parent), mBackgroundColor(0,0,255), mShapeColor(255,255,255)
+{
+
+}
+
+QSize RenderArea::minimumSizeHint() const
+{
+    return QSize(100, 100);
+}
+QSize RenderArea::sizeHint() const
+{
+    return QSize(400, 200);
+}
+
+void RenderArea::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+    painter.setBrush(mBackgroundColor);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setPen(mShapeColor);
+
+    //drawing area
+    painter.drawRect(this->rect());
+    painter.drawLine(this->rect().topLeft(),this->rect().bottomRight());
+}
